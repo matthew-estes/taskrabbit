be48825a5e9931aa2f9f68133959b3f597456061
matthew-estes <matthew.estes19@gmail.com>
2024-06-12 10:47:12 -0400

    move taskSchema to user.js file to streamline

--- models/user.js
+++ models/user.js
@@ -1,5 +1,18 @@
 const mongoose = require('mongoose');
 
+const taskSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String },
+  category: { type: String, required: true },
+  isHighPriority: { type: Boolean, required: true },
+  dueDate: { type: Date, required: true },
+  status: {
+    type: String,
+    enum: ["Not Started", "In Progress", "Completed", "On Hold", "Cancelled"],
+    required: true,
+  },
+});
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -9,8 +22,10 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  tasks: [taskSchema],
 });
 
 const User = mongoose.model('User', userSchema);
+
 
 module.exports = User;
