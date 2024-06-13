8f97eb3610c31a15cdc75430e322168165907029
matthew-estes <matthew.estes19@gmail.com>
2024-06-13 10:23:55 -0400

    update tasks route with delete

--- routes/tasks.js
+++ routes/tasks.js
@@ -6,5 +6,6 @@ router.get("/", taskCtrl.index);
 router.get("/new", taskCtrl.new);
 router.post("/", taskCtrl.create);
 router.get("/:taskId", taskCtrl.show);
+router.delete('/:taskId', taskCtrl.delete)
 
 module.exports = router;
