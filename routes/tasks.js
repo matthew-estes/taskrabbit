a4e09e50122aa568a0bbf08bc656b7e76686abe1
matthew-estes <matthew.estes19@gmail.com>
2024-06-13 10:32:16 -0400

    update routes with edit/update

--- routes/tasks.js
+++ routes/tasks.js
@@ -6,6 +6,8 @@ router.get("/", taskCtrl.index);
 router.get("/new", taskCtrl.new);
 router.post("/", taskCtrl.create);
 router.get("/:taskId", taskCtrl.show);
-router.delete('/:taskId', taskCtrl.delete)
+router.delete('/:taskId', taskCtrl.delete);
+router.get('/:taskId/edit', taskCtrl.edit);
+router.put('/:taskId', taskCtrl.update);
 
 module.exports = router;
